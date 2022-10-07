d2c8a6545f726f77d4ae22f638fbaddeb04ddc33
orji deborah <orjideborahesther@gmail.com>
2022-10-07 23:02:03 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,54 @@
+export const projects = [
+  {
+    title: "Cloned Website",
+    subtitle: "Netflix clone",
+    description:
+      "",
+    image: "./netflix-netflix-logo.gif",
+    link: "https://netflix.com",
+  },
+  {
+    title: "Payment Gateway",
+    subtitle: "Cloned Payment Gateway",
+    description:
+      "",
+    image: "./paystack.gif",
+    link: "https://Paystack.com",
+  },
+  {
+    title: "Calculator",
+    subtitle: "Calculator",
+    description:
+      "",
+    image: "./calculator-app-1_1.gif",
+    link: "",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "",
+    image: "./0.gif",
+    link: "",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "She built a really cute To do App.",
+    image: "",
+    name: "Nkechi Amadi",
+    company: "",
+  },
+  
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
